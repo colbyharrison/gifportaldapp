2b16ea406e6ec3582c617a966639b165c50301f0
Colby Harrison <colbyharrison@Colbys-Mac-mini.home>
2022-01-03 11:49:01 -0800

    update tests

--- tests/gifportaldapp.ts
+++ tests/gifportaldapp.ts
@@ -11,6 +11,9 @@ import AccountFactory from '@project-serum/anchor/dist/cjs/program/namespace/acc
 
 const { SystemProgram } = anchor.web3;
 
+
+
+
 describe('gifportaldapp', () => {
 
   const provider = anchor.Provider.env();
@@ -18,6 +21,9 @@ describe('gifportaldapp', () => {
   anchor.setProvider(provider);
   const baseAccount = anchor.web3.Keypair.generate();
   const program = anchor.workspace.Gifportaldapp as Program<Gifportaldapp>;
+
+  const gifContributor = anchor.web3.Keypair.generate();
+
 
   it('Is initialized!', async () => {
     await program.rpc.initialize({
@@ -30,12 +36,14 @@ describe('gifportaldapp', () => {
     });
 
     const account = await program.account.baseAccount.fetch(baseAccount.publicKey);
+
     assert.ok(account.totalGifs.eq(new anchor.BN(0)))
   });
 
   it('Gif added', async () => {
 
     const gifLink = 'insert_a_giphy_link_here';
+
     await program.rpc.addGif(gifLink, {
       accounts: {
         baseAccount: baseAccount.publicKey,
@@ -45,7 +53,6 @@ describe('gifportaldapp', () => {
 
     const account = await program.account.baseAccount.fetch(baseAccount.publicKey);
 
-    console.log(account);
     assert.ok(account.totalGifs.eq(new anchor.BN(1)))
     assert.equal(account.gifList[0].gifLink, gifLink);
     assert.ok(account.gifList[0].id.eq(new anchor.BN(0)));
@@ -53,9 +60,28 @@ describe('gifportaldapp', () => {
 
   });
 
+
+  it('Second Gif Added', async () => {
+    const gifLink = 'second link';
+    await program.rpc.addGif(gifLink, {
+      accounts: {
+        baseAccount: baseAccount.publicKey,
+        user: provider.wallet.publicKey,
+      },
+    });
+
+    const account = await program.account.baseAccount.fetch(baseAccount.publicKey);
+
+    assert.ok(account.totalGifs.eq(new anchor.BN(2)))
+    assert.equal(account.gifList[1].gifLink, gifLink);
+    assert.ok(account.gifList[1].id.eq(new anchor.BN(1)));
+    assert.equal(Object.keys(account.gifList).length, 2);
+  });
+
   it('Gif upvoted', async () => {
 
     const gifId = '0';
+
     await program.rpc.incrementUpVote(gifId, {
       accounts: {
         baseAccount: baseAccount.publicKey,
@@ -64,24 +90,23 @@ describe('gifportaldapp', () => {
     });
 
     const account = await program.account.baseAccount.fetch(baseAccount.publicKey);
+
     assert.ok(account.gifList[0].upVotes.eq(new anchor.BN(1)));
   });
 
   it('Tip gif poster', async () => {
     const gifLink = 'insert_a_giphy_link_here';
+    let sendAmount = anchor.web3.LAMPORTS_PER_SOL;
 
-    const gifOwner = anchor.web3.Keypair.generate();
-
-    let sendAmount = anchor.web3.LAMPORTS_PER_SOL * 10
     await program.rpc.tip(sendAmount.toString(), {
       accounts: {
         from: provider.wallet.publicKey,
-        to: gifOwner.publicKey,
+        to: gifContributor.publicKey,
         systemProgram: SystemProgram.programId
       }
     });
 
-    const gifOwnerBalance = await provider.connection.getBalance(gifOwner.publicKey);
-    assert.equal(sendAmount, gifOwnerBalance)
+    const gifOwnerBalance = await provider.connection.getBalance(gifContributor.publicKey);
+    assert.equal(sendAmount, gifOwnerBalance); //fails does not account for fees?
   });
 });
